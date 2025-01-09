d285d3da8802ab5b4868f258edef98681d30375f
Fernando <washo968@gmail.com>
2025-01-09 05:40:59 -0500

    Primer Commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,53 @@
+const {response} = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+    const {q,nombre='No name',apikey,page=1,limit} = req.query;
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    const {q,nombre='No name',apikey,page = 1,limit} = req.body;
+    res.json({
+        msg: 'post API - usuariosPost',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    const {id} = req.params;
+    res.json({
+        msg: 'put API - usuariosPut',
+        id
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete, 
+}
